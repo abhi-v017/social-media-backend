a3585ab8bc39ab2f10f7b6a704768b2b32bea430
abhishek verma <abhi.av017@gmail.com>
2025-02-18 13:18:23 +0530

    completion of backend

--- src/controllers/followers.controller.js
+++ src/controllers/followers.controller.js
@@ -3,6 +3,8 @@ import { User } from "../models/user.model.js";
 import { Follow } from "../models/follow.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
+import { getMongoosePaginationOptions } from '../utils/getMongoosePaginationOptions.js'
 
 
 const followUnFollowUser = asyncHandler(async (req, res) => {
@@ -60,18 +62,326 @@ const followUnFollowUser = asyncHandler(async (req, res) => {
     }
 });
 
-const getFollowerListByUsername = asyncHandler(async (req, res) => {
+const getFollowersListByUserName = asyncHandler(async (req, res) => {
     const { username } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
-    const user = await User
-        .findOne({ username })
-        .select("_id");
+    const userAggregation = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase(),
+            },
+        },
+        {
+            $lookup: {
+                // lookup for the each user's profile
+                from: "users",
+                localField: "_id",
+                foreignField: "owner",
+                as: "profile",
+                pipeline: [
+                    {
+                        $project: {
+                            firstName: 1,
+                            lastName: 1,
+                            bio: 1,
+                            location: 1,
+                            coverImage: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: { profile: { $first: "$profile" } },
+        },
+        {
+            $project: {
+                username: 1,
+                email: 1,
+                avatar: 1,
+                profile: 1,
+            },
+        },
+    ]);
+
+    const user = userAggregation[0];
 
     if (!user) {
-        throw new ApiError(404, "User not found");
+        throw new ApiError(404, "User does not exist");
     }
-    
+    const userId = user._id;
+    const followersAggregate = Follow.aggregate([
+        {
+            $match: {
+                followeeId: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        // Now we have all the follow documents where current user is followee (who is being followed)
+        {
+            $lookup: {
+                // Lookup for the followers (users which are following current users)
+                from: "users",
+                localField: "followerId",
+                foreignField: "_id",
+                as: "follower",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "_id",
+                            foreignField: "owner",
+                            as: "profile",
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: "follows",
+                            localField: "_id",
+                            foreignField: "followeeId",
+                            as: "isFollowing",
+                            pipeline: [
+                                {
+                                    $match: {
+                                        followerId: new mongoose.Types.ObjectId(req.user?._id), // Only get documents where logged in user is the follower
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        $addFields: {
+                            profile: { $first: "$profile" },
+                            isFollowing: {
+                                $cond: {
+                                    if: {
+                                        $gte: [
+                                            {
+                                                // if the isFollowing key has document in it
+                                                $size: "$isFollowing",
+                                            },
+                                            1,
+                                        ],
+                                    },
+                                    then: true,
+                                    else: false,
+                                },
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            username: 1,
+                            email: 1,
+                            avatar: 1,
+                            profile: 1,
+                            isFollowing: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                follower: { $first: "$follower" },
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                follower: 1,
+            },
+        },
+        {
+            $replaceRoot: {
+                newRoot: "$follower",
+            },
+        },
+    ]);
+
+    const followersList = await Follow.aggregatePaginate(
+        followersAggregate,
+        getMongoosePaginationOptions({
+            page,
+            limit,
+            customLabels: {
+                totalDocs: "totalFollowers",
+                docs: "followers",
+            },
+        })
+    );
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { user, ...followersList },
+                "Followers list fetched successfully"
+            )
+        );
+});
+const getFollowingListByUserName = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    const { page = 1, limit = 10 } = req.query;
+
+    const userAggregation = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase(),
+            },
+        },
+        {
+            $lookup: {
+                // lookup for the each user's profile
+                from: "users",
+                localField: "_id",
+                foreignField: "owner",
+                as: "profile",
+                pipeline: [
+                    {
+                        $project: {
+                            firstName: 1,
+                            lastName: 1,
+                            bio: 1,
+                            location: 1,
+                            coverImage: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: { profile: { $first: "$profile" } },
+        },
+        {
+            $project: {
+                username: 1,
+                email: 1,
+                avatar: 1,
+                profile: 1,
+            },
+        },
+    ]);
+
+    const user = userAggregation[0];
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist");
+    }
+
+    const userId = user._id;
+    const followingAggregate = Follow.aggregate([
+        {
+            $match: {
+                followerId: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $lookup: {
+                 from: "users",
+                localField: "followeeId",
+                foreignField: "_id",
+                as: "following",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "_id",
+                            foreignField: "owner",
+                            as: "profile",
+                        },
+                    },
+                     {
+                        $lookup: {
+                            from: "follows",
+                            localField: "_id",
+                            foreignField: "followeeId",
+                            as: "isFollowing",
+                            pipeline: [
+                                {
+                                    $match: {
+                                        followerId: new mongoose.Types.ObjectId(req.user?._id), // Only get documents where logged in user is the follower
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        $addFields: {
+                            profile: { $first: "$profile" },
+                            isFollowing: {
+                                $cond: {
+                                    if: {
+                                        $gte: [
+                                            {
+                                                $size: "$isFollowing",
+                                            },
+                                            1,
+                                        ],
+                                    },
+                                    then: true,
+                                    else: false,
+                                },
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            username: 1,
+                            email: 1,
+                            avatar: 1,
+                            profile: 1,
+                            isFollowing: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                following: { $first: "$following" },
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                following: 1,
+            },
+        },
+        {
+            $replaceRoot: {
+                newRoot: "$following",
+            },
+        },
+    ]);
+
+    const followingList = await Follow.aggregatePaginate(
+        followingAggregate,
+        getMongoosePaginationOptions({
+            page,
+            limit,
+            customLabels: {
+                totalDocs: "totalFollowing",
+                docs: "following",
+            },
+        })
+    );
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { user, ...followingList },
+                "Following list fetched successfully"
+            )
+        );
 });
 
 
-export { followUnFollowUser };
+
+export {
+    followUnFollowUser,
+    getFollowersListByUserName,
+    getFollowingListByUserName
+};
--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {verifyJwt} from "../middlewares/auth.middleware.js"
-import { followUnFollowUser } from "../controllers/followers.controller.js";
+import { followUnFollowUser, getFollowersListByUserName,getFollowingListByUserName  } from "../controllers/followers.controller.js";
 
 const router = Router()
 
 
 router.route("/follow/:userId").post(verifyJwt, followUnFollowUser);
+router.route("/followers-list/:username").get(verifyJwt, getFollowersListByUserName);
+router.route("/following-list/:username").get(verifyJwt, getFollowingListByUserName);
 
 export default router
